4f6240502acfe0146c86addebcf3fd2648b8f780
Walid <hello@walidvb.com>
2021-12-08 08:30:02 -0300

    working home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,20 +8,6 @@ import { mapLinksToRenderable } from '../src/api/helpers/mapLinksToRenderable';
 export default function App({ links }) {
 
   return (
-    <MediaProvider list={links}>
-      <View style={tw`pb-16 container mx-auto`}>
-        <MediaQueue
-          withTimeSeparators
-          type="grid"
-        />
-        <CurrentMediaPlayer style={[tw`bottom-1 right-1 border p-2 z-10 bg-white`, { position: 'fixed'}]} />
-      </View>
-    </MediaProvider>
+    <h1>Welcome back!</h1>
   );
-}
-
-export const getStaticProps = async () => {
-  const { data: { links } } = await axios.get("http://localhost:3000/api/links") ;
-  console.log(links)
-  return { props: { links: mapLinksToRenderable(links) } };
 }
