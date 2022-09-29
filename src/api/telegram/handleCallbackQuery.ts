c5b0c16bca6ec0e3655e92245a9604a7ea23f836
Walid <hello@walidvb.com>
2022-09-29 18:31:54 +0200

    delete no thank smsg

--- src/api/telegram/handleCallbackQuery.ts
+++ src/api/telegram/handleCallbackQuery.ts
@@ -8,20 +8,21 @@ export const handleCallbackQuery = async (ctx: Context) => {
   const { data, message: { reply_to_message: originalMsg } } = callback_query
   if (data === 'No') {
     await ctx.editMessageText(`Ok, skipping...`)
-    return
   }
-  console.log("going here?!?")
-  try{
-    await ctx.editMessageText(`Cool, saving...`)
-  } catch(err){
-    console.log('Error editing message: ', JSON.stringify(err, null, 2))
+  else{
+    try {
+      await ctx.editMessageText(`Cool, saving...`);
+    } catch (err) {
+      console.log('Error editing message: ', JSON.stringify(err, null, 2));
+    }
+    try {
+      await addLinkFromMsg(originalMsg, ctx);
+    } catch (err) {
+      console.log('Error adding link: ', JSON.stringify(err, null, 2));
+    }
+    await ctx.editMessageText(`Link Saved! Use /play to view all posts`);
   }
-  try{
-    await addLinkFromMsg(originalMsg, ctx)
-  } catch(err){
-    console.log('Error adding link: ', JSON.stringify(err, null, 2))
-  }
-  await ctx.editMessageText(`Link Saved! Use /play to view all posts`);
+
   await new Promise((res, rej) => {
     setTimeout(async () => {
       try {
