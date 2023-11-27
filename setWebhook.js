cf509799f70efdd82dad462c561a5493f2fafd69
Walid <hello@walidvb.com>
2023-11-27 17:50:01 +0000

    fix setWebhook

--- setWebhook.js
+++ setWebhook.js
@@ -5,10 +5,9 @@ dotenv.config();
 const setWebhook = async () => {
     const domain = process.env.DOMAIN || process.env.VERCEL_URL
     const response = await fetch(
-      `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/setWebhook?url=https://${url}/api/telegram`
+      `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/setWebhook?url=https://${domain}/api/telegram`
     );
     const data = await response.json();
-    console.log(data);
     return data;
 }
 
