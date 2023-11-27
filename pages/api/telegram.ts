9318beeae0933fa8aabf2bea3b3e32aebcf52b92
Walid <hello@walidvb.com>
2023-11-27 18:23:57 +0000

    restore nextjs

--- pages/api/telegram.ts
+++ pages/api/telegram.ts
@@ -34,7 +34,7 @@ bot.on('message', wrapInTryCatch(async (ctx: Context) => {
   console.log('message', JSON.stringify(ctx.msg, null, 2))
 }))
 
-export default webhookCallback(bot, 'express', undefined, 5_000)
+export default webhookCallback(bot, 'next-js', undefined, 5_000)
 
 type wrapped = (ctx: Context) => void
 function wrapInTryCatch(func: (ctx: Context) => void): wrapped{
